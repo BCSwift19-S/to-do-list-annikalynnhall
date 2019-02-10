d5c56f54d87b677fb7b65e40dd5c2aaf5b7e94d8
Annika <annikahall@annikas-macbook-pro.local>
2019-02-10 13:33:11 -0500

    5.2 Completed: Navigation, Segues, Bar Button Items, Cancel Button

---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Annika Lynn Nordstrom Hall on 2/10/19.
+//  Copyright © 2019 Annika Hall. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        }else{
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
